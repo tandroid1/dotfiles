404cb7ba5c0433250e846f9f83516af2dae6dad7
gabesullice <gabriel@sullice.com>
2014-04-27 12:09:01 -0600

    Fixes multiple branch line wrapping prompt.

--- git/source_git.bash
+++ git/source_git.bash
@@ -2,7 +2,7 @@ source ~/.dotfiles/git/git-flow-completion/git-flow-completion.bash
 source ~/.dotfiles/git/git-completion/git-completion.bash
 
 function parse_git_branch {
-  branch=`git branch 2> /dev/null | awk '{print $2}' | cut -c 1-3`
+  branch=`git branch 2> /dev/null | grep '^*' | awk '{print $2}' | cut -c 1-3`
   if [ ! -z $branch ]; then
     echo ": $branch "
   fi
