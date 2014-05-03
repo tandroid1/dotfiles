dd73046adbbe37649fd817ba6586e84428100167
gabesullice <gabriel@sullice.com>
2014-05-03 15:29:07 -0600

    Adds cd .. aliases.

--- aliases/source_aliases.bash
+++ aliases/source_aliases.bash
@@ -1,2 +1,7 @@
 source ~/.dotfiles/aliases/development/source_gems.bash
 source ~/.dotfiles/aliases/development/source_utilities.bash
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
