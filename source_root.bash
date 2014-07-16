c9f5172358e33072b9ccc9054f5d5d2d20ff03a9
gabesullice <gabriel@sullice.com>
2014-07-16 17:40:53 -0600

    Switches out ~ for /Users/gabesullice in root sourcing file.

--- source_root.bash
+++ source_root.bash
@@ -1,9 +1,9 @@
 # Creates quick alias to get to cd the dotfiles directory.
 alias dotfiles="cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source ~/.dotfiles/aliases/source_aliases.bash
-source ~/.dotfiles/bash/source_bash.bash
-source ~/.dotfiles/drupal/source_drupal.bash
-source ~/.dotfiles/git/source_git.bash
-source ~/.dotfiles/terminal/source_terminal.bash
-source ~/.dotfiles/vim/source_vim.bash
+source $HOME/.dotfiles/aliases/source_aliases.bash
+source $HOME/.dotfiles/bash/source_bash.bash
+source $HOME/.dotfiles/drupal/source_drupal.bash
+source $HOME/.dotfiles/git/source_git.bash
+source $HOME/.dotfiles/terminal/source_terminal.bash
+source $HOME/.dotfiles/vim/source_vim.bash
