133c1fd3250e55d5bb75b23b8daada0106444ff7
gabesullice <gabriel@sullice.com>
2014-07-16 18:29:57 -0600

    Refactors setup.sh script.

--- setup.sh
+++ setup.sh
@@ -1,97 +1,48 @@
 #/usr/bin/bash
 
-if [ -h ~/.vim ]
-then 
-  rm ~/.vim
-  ln -s ~/.dotfiles/vim/.vim ~/.vim
-  echo "Replaced ~/.vim symlink with symlink to the .dotfiles .vim directory."
-elif [ -f ~/.vim ]
-then 
-  mv ~/.vim ~/.vim-bkp
-  ln -s ~/.dotfiles/vim/.vim ~/.vim
-  echo "Moved ~/.vim to .vim-bkp and created new ~/.vim symlink to the .dotfiles .vim directory."
-else
-  ln -s ~/.dotfiles/vim/.vim ~/.vim
-  echo "Created new ~/.vim symlink to the .dotfiles .vim directory."
-fi
+setup_dir() {
+  if [ ! -d $1 ]
+  then
+    mkdir $1
+    echo "Created $1 directory."
+  else
+    echo "Creating $1 failes. Directory already exists."
+  fi
+}
 
-if [ ! -d ~/.vimbackups ]
-then
-  mkdir ~/.vimbackups
-  echo "Created .vimbackups directory."
-fi
+setup_dotfile() {
+  dotfile=$1
+  target=$2
+  if [ -h $target ]
+  then 
+    rm $target
+    ln -s $dotfile $target
+    echo "Replaced $target symlink with symlink to $dotfile." 
+  elif [ -f $target ]
+  then 
+    mv $target $HOME/dotfile-bkp/
+    ln -s $dotfile $target
+    echo "Moved $target to $HOME/dotfile-bkp/ and created new symlink to $dotfile."
+  else
+    ln -s $dotfile $target
+    echo "Created new $target symlink to $dotfile."
+  fi
+}
 
-if [ -h ~/.vimrc ]
-then 
-  rm ~/.vimrc
-  ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
-  echo "Replaced ~/.vimrc symlink with symlink to the .dotfiles .vimrc file."
-elif [ -f ~/.vimrc ]
-then 
-  mv ~/.vimrc ~/.vimrc-bkp
-  ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
-  echo "Moved ~/.vimrc to .vimrc-bkp and created new ~/.vimrc symlink to the .dotfiles .vimrc file."
-else
-  ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
-  echo "Created new ~/.vimrc symlink to the .dotfiles .vimrc file."
-fi
+# Global configurations
+setup_dir $HOME/dotfile-bkp
 
-if [ -h ~/.git_template ]
-then 
-  rm ~/.git_template
-  ln -s ~/.dotfiles/git/git_template ~/.git_template
-  echo "Replaced ~/.git_template symlink with symlink to the .dotfiles git_template directory."
-elif [ -f ~/.git_template ]
-then 
-  mv ~/.git_template ~/.git_template-bkp
-  ln -s ~/.dotfiles/git/git_template ~/.git_template
-  echo "Moved ~/.git_template to .git_template-bkp and created new ~/.git_template symlink to the .dotfiles git_template directory."
-else
-  ln -s ~/.dotfiles/git/git_template ~/.git_template
-  echo "Created new ~/.git_template symlink to the .dotfiles git_template directory."
-fi
+# Vim configurations
+setup_dir $HOME/.vimbackups 
+setup_dotfile $HOME/.dotfiles/vim/.vim $HOME/.vim 
+setup_dotfile $HOME/.dotfiles/vim/.vimrc $HOME/.vimrc 
 
-if [ -h ~/.gitconfig ]
-then 
-  rm ~/.gitconfig
-  ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
-  echo "Replaced ~/.gitconfig symlink with symlink to the .dotfiles .gitconfig file."
-elif [ -f ~/.gitconfig ]
-then 
-  mv ~/.gitconfig ~/.gitconfig-bkp
-  ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
-  echo "Moved ~/.gitconfig to .gitconfig-bkp and created new ~/.gitconfig symlink to the .dotfiles .gitconfig file."
-else
-  ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
-  echo "Created new ~/.gitconfig symlink to the .dotfiles .gitconfig file."
-fi
 
-if [ -h ~/.gitignore ]
-then 
-  rm ~/.gitignore
-  ln -s ~/.dotfiles/git/gitignore ~/.gitignore
-  echo "Replaced ~/.gitignore symlink with symlink to the .dotfiles .gitignore file."
-elif [ -f ~/.gitignore ]
-then 
-  mv ~/.gitignore ~/.gitignore-bkp
-  ln -s ~/.dotfiles/git/gitignore ~/.gitignore
-  echo "Moved ~/.gitignore to .gitignore-bkp and created new ~/.gitignore symlink to the .dotfiles .gitignore file."
-else
-  ln -s ~/.dotfiles/git/gitignore ~/.gitignore
-  echo "Created new ~/.gitignore symlink to the .dotfiles .gitignore file."
-fi
+# Git configuration
+setup_dotfile $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig 
+setup_dotfile $HOME/.dotfiles/git/.gitignore $HOME/.gitignore 
+setup_dotfile $HOME/.dotfiles/git/.git_template $HOME/.git_template 
 
-if [ -h ~/.bashrc ]
-then 
-  rm ~/.bashrc
-  ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
-  echo "Replaced ~/.bashrc symlink with symlink to the .dotfiles .bashrc file."
-elif [ -f ~/.bashrc ]
-then 
-  mv ~/.bashrc ~/.bashrc-bkp
-  ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
-  echo "Moved ~/.bashrc to .bashrc-bkp and created new ~/.bashrc symlink to the .dotfiles .bashrc file."
-else
-  ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
-  echo "Created new ~/.bashrc symlink to the .dotfiles .bashrc file."
-fi
+# Bash configuration
+setup_dotfile $HOME/.dotfiles/bash/.bashrc $HOME/.bashrc 
+
