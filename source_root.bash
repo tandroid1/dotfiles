e610ec719f22c796ce90758dac70a9788e44eaf1
gabesullice <gabriel@sullice.com>
2014-07-16 19:34:18 -0600

    Removes unecessary drupal source file and reference.

--- source_root.bash
+++ source_root.bash
@@ -1,4 +1,3 @@
-source $HOME/.dotfiles/drupal/source_drupal.bash
 source $HOME/.dotfiles/git/source_git.bash
 source $HOME/.dotfiles/terminal/source_terminal.bash
 source $HOME/.dotfiles/vim/source_vim.bash
