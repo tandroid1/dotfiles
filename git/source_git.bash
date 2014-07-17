018d5ff90f610e66e157bd8ece787224faf54be2
gabesullice <gabriel@sullice.com>
2014-07-16 19:35:44 -0600

    Swaps ~ for /Users/gabesullice env var.

--- git/source_git.bash
+++ git/source_git.bash
@@ -1,2 +1,2 @@
-source ~/.dotfiles/git/git-flow-completion/git-flow-completion.bash
-source ~/.dotfiles/git/git-completion/git-completion.bash
+source $HOME/.dotfiles/git/git-flow-completion/git-flow-completion.bash
+source $HOME/.dotfiles/git/git-completion/git-completion.bash
