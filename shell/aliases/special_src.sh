6f200cc69dd830189e4b7e1b91ef5465034f1ac5
gabesullice <gabriel@sullice.com>
2014-07-17 09:28:48 -0600

    Adds alias for vim and moves addvhost alias from special to utils.

--- shell/aliases/special_src.sh
+++ shell/aliases/special_src.sh
@@ -1,3 +1,11 @@
+# These aliases not directly related to the shell, like git, vim, compass etc.
+
+# Git aliases. Note: git command aliases should go in your .gitconfig file.
+alias g="git"
+
+# Aliases vim
+alias v="vim"
+
 # Creates quick alias to get to cd the dotfiles directory.
 alias dotfiles="cd $HOME/.dotfiles"
 
@@ -10,21 +18,3 @@ alias vim_hosts="sudo vim /etc/hosts"
 alias vim_vhosts="vim /Applications/MAMP/conf/apache/extra/httpd-vhosts.conf"
 # Drupal alias folder
 alias vim_alias="vim ~/.drush"
-
-# Add a new vhost to your vhost file with parameter one as the filename and as servername.
-addvhost() {
-echo -e "# $1
-<VirtualHost *:80>
-    DocumentRoot "/Users/gabesullice/development/docroot/$1/site"
-    ServerName $1.local
-    ServerAlias www.$1.local
-    <Directory /Users/gabesullice/development/docroot/$1/site/>
-        Options Indexes FollowSymlinks
-        AllowOverride All
-        Order allow,deny
-        Allow from all
-    </Directory>
-</VirtualHost>\n" >> /Applications/MAMP/conf/apache/extra/httpd-vhosts.conf
-}
-alias addvhost=addvhost
-
