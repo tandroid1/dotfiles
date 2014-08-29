ed1450a58684b4ea9fa689e7730c965693ea0e44
gabesullice <gabriel@sullice.com>
2014-08-29 14:45:23 -0600

    Add mkcd alias

--- shell/aliases/utils_src.sh
+++ shell/aliases/utils_src.sh
@@ -1,3 +1,10 @@
+# mkcd makes a dir and immediately cd's into it
+_mkcd() {
+  mkdir $1
+  cd $1
+}
+alias mkcd=_mkcd
+
 # MAC ONLY
 # Copy a file passed as the first parameter to the clipboard
 catcopy() {
