33bb67149c8c2a7ecb0298d54a0350dadc6863bc
gabesullice <gabriel@sullice.com>
2015-01-23 09:31:40 -0700

    Add fucking sudo alias

--- shell/aliases/common_src.sh
+++ shell/aliases/common_src.sh
@@ -9,6 +9,9 @@ alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
+# Fucking sudo
+alias fucking="sudo"
+
 # prints the full path to a file, like pwd, for files
 function _pwf () {
 dir=$(echo $(cd $(dirname "$1"); pwd))
